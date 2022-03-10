8187d88cde5fda2217842a7c954f97b74e11c109
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-03-10 13:14:22 +0000

    :memo: create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+// サンプルプログラム　Main
+public class Main{
+  //ここからmainメソッド
+  public static void main(String[]args){
+    int age; //年齢を入れる箱
+    age = 20;
+    System.out.println("私は" + age + "歳");
+  }
+}
