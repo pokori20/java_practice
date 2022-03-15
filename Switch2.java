f2be9aba98d4dc095270f76f04fe2f7309ac52ee
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-03-15 11:09:31 +0000

    :memo: create Switch2
    break文をあえて書かない構文

---
+++ Switch2.java
@@ -0,0 +1,19 @@
+//あえてbreakを書かない構文
+public class Switch2 {
+  public static void main (String[] args) {
+  System.out.println("あなたの運勢を占います");
+  int fortune = new java.util.Random().nextInt(5) + 1;
+  switch (fortune) {
+    case 1:
+    case 2:
+      System.out.println("いいね");
+      break;
+    case 3:
+      System.out.println("普通です");
+      break;
+    case 4:
+    case 5:
+      System.out.println("うーん");
+    }
+  }
+}
