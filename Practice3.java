232e6ee030222b9515a8fea8162f137064dc0041
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-03-18 11:29:32 +0000

    :sparkles: create Practice36.java
    for文を用いた数当てゲーム

---
+++ Practice3.java
@@ -0,0 +1,19 @@
+//練習問題3.8
+public class Practice3 {
+  public static void main (String[] args) {
+    // int weight == 60;
+    // int (age1 + age2) * 2 > 60;
+    // int age % 2 == 1
+    // String name.equals "渚";
+    int seibetsu = 0;
+    int age = 26;
+    System.out.println("こんにちは");
+    if (seibetsu == 0) {
+      System.out.println("私は男です");
+      System.out.println(age + "歳です");
+    }else{
+      System.out.println("私は女です");
+    }
+    System.out.println("よろしくお願いします。");
+  }
+}
