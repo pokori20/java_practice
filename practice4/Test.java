f7a51abe087989bbc1f2e82153a2c914130dd096
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-03-22 09:42:40 +0000

    :memo: create test
    4章テスト問題

---
+++ practice4/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+  public static void main (String[] args) {
+    //練習4-1
+    int[] points = new int[4];
+    double[] weights = new double[5];
+    boolean[] answers = new boolean[3];
+    String[] names = new String[3];
+    //練習4-2
+    int[] moneyList = { 121902, 8302, 55100};
+    for (int i = 0; i < moneyList.length; i++)
+      System.out.println(moneyList[i]);
+    fot (int value : moneyList) {
+      System.out.println(value);
+    }
+  }
+}
