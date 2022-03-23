01978a2bf07aa4bea064c205f5b2368f5c175aeb
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-03-23 08:42:35 +0000

    :memo: edited Test
    edited Test

--- practice4/Test.java
+++ practice4/Test.java
@@ -1,16 +1,26 @@
 public class Test {
-  public static void main (String[] args) {
-    //練習4-1
+  public static void main(String[] args) {
+    // //練習4-1
     int[] points = new int[4];
     double[] weights = new double[5];
     boolean[] answers = new boolean[3];
     String[] names = new String[3];
     //練習4-2
-    int[] moneyList = { 121902, 8302, 55100};
-    for (int i = 0; i < moneyList.length; i++)
+    int[] moneyList = { 121902, 8302, 55100 };
+    for (int i = 0; i < moneyList.length; i++) {
       System.out.println(moneyList[i]);
-    fot (int value : moneyList) {
-      System.out.println(value);
+    }
+    for (int m : moneyList) {
+      System.out.println(m);
+    }
+      //練習4
+    int[] numbers = {3, 4, 9};
+    System.out.println("1桁の数字を入力してください");
+    int input = new java.util.Scanner(System.in).nextInt();
+    for(int n : numbers) {
+      if (n == input) {
+        System.out.println("アタリ!");
+      }
     }
   }
 }
