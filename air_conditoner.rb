aa2c86c4a13038215e476b4f257bd584e878ee1c
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-03-30 11:48:29 +0000

    :memo: programming test
    プログラミングテスト回答コード

--- air_conditoner.rb
+++ air_conditoner.rb
@@ -4,6 +4,8 @@ input_lines = readlines
 room = input_lines[0].to_i
 # エアコン温度
 air_conditoner = input_lines[1].to_i
+# 風量
+power = input_lines[2].to_i
 # 温度差
 temperature_diff = (room - air_conditoner).abs
 # 所要時間変数の初期化
@@ -16,5 +18,19 @@ elsif temperature_diff >= 5 && temperature_diff < 10 then
 elsif temperature_diff >= 10
   require_time = 60
 end
+
+if power == 1
+  total = require_time
+elsif power == 2
+  total = require_time - 5
+elsif power == 3
+  total = require_time - 10
+else
+  p "1~3を入力してください。"
+  exit
+end
+
+
 # 所要時間表示
-p require_time
+p total
+puts "分"
---
+++ test.rb
@@ -0,0 +1,15 @@
+# 標準入力受付
+# num = gets.chomp.split(" ").map(&:to_i)
+
+# x = num[0]
+# # 1回ずつカウント
+# i = 1
+
+# array = []
+# # iが10回を超えるまで繰り返し
+# while i <= 10
+#   array << x
+#   x = x + num[1]
+#   i += 1
+# end
+# print array.join(" ")
