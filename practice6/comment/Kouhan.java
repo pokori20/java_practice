dafcbfb834cc8da6de5b79651057a1a6e458dcc2
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-04-01 07:12:49 +0000

    :sparkles: finish test6
    6章テスト終わり

--- practice6/comment/Kouhan.java
+++ practice6/comment/Kouhan.java
@@ -6,6 +6,9 @@ public class Kouhan {
   public static void showMondokoro() throws Exception {
     System.out.println("飛車さん、角さん。もういいでしょう。");
     System.out.println("この紋所が目に入らぬか！");
+    // sleepメソッドの引数はlong型、単位はミリ秒(1000ミリ秒 = 1秒)
+    Thread.sleep(3000);
+    System.out.println("一時停止確認");
     // Zenhan.doTogame();
   }
 }
