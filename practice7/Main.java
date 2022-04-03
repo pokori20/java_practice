6dcd47848a7b4db8524b625378f98203a759ca60
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-04-03 11:33:10 +0000

    :construction: Run yuusya program
    mainメソッドに勇者プログラムを定義して実行する

--- practice7/Main.java
+++ practice7/Main.java
@@ -2,8 +2,23 @@
 public class Main {
   public static void main(String[] args) {
     //以下の内容をJavaで記述していく
-    //勇者よ、この仮想世界に生まれよ！
+    //1.勇者よ、この仮想世界に生まれよ！
+    Hero yuusya = new Hero();
+    //2.フィールドに初期値をセット
+    yuusya.name = "ミナト";
+    yuusya.hp = 100;
+    System.out.println("勇者" + yuusya.name + "を生み出しました");
+    //3.勇者のメソッドを呼び出してゆく
+    //5秒座れ
+    yuusya.sit(5);
+    //転べ
+    yuusya.slip();
+    //座れ
+    yuusya.sit(10);
+    //逃げろ
+    yuusya.run();
     //お化けキノコよ、この仮想世界に生まれよ！
+    //Matango monster = new Matango();
     //勇者よ、戦え！
     //お化けキノコよ、逃げろ
   }
