9d52ed51336f9b0edc2242941338e13a443f9cdc
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-04-30 14:48:49 +0000

    :construction: learning interface
    learning interface

---
+++ practice11/Circle.java
@@ -0,0 +1,4 @@
+//インターフェースにおける定数宣言
+public interface Circle {
+  double PI = 3.141592; //自動的にpublic static finalが補われる
+}
---
+++ practice11/Creature.java
@@ -0,0 +1,4 @@
+//インターフェースについて　１．すべてのメソッドは抽象メソッドかつ 2.基本的にフィールドを1つも持たない。
+public interface class Creature { //インターフェースとして宣言
+  void run(); //public abstract を省略してもよい
+}
