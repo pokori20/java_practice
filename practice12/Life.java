2199f5d84529d7a2c946e977114155bfc5604306
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-05-05 13:30:29 +0000

    :construction: start practice12
    start practice12

---
+++ practice12/Life.java
@@ -0,0 +1,4 @@
+//12-1　箱の型に抽象クラスを使う
+public interface Life {
+  
+}
---
+++ practice12/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    //インターフェースであるLifeはインスタンス生成のためには利用できない。しかし、Lifeという変数の箱として使用できる。
+    Life lf = new Wizard;
+  }
+}
