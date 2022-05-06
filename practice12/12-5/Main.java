36bce79158a9187af1c613b6e0cf62f7b87b89b6
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-05-06 13:34:48 +0000

    :construction: fuzzy mold box
    曖昧な方の箱へのインスタンスの代入

---
+++ practice12/12-5/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    Slime s = new Slime();
+    Monster m = new Slime();
+    s.run();
+    m.run();
+    //s.runとm.runはどのように表示されるか？
+    //A.いづれもスライムはサササっと逃げ出した！と表示される。
+  }
+}
