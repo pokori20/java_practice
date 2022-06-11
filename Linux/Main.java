fca44385340eeb4fd1a575e616fc37579a9c84f5
pokori20 <93557699+pokori20@users.noreply.github.com>
2022-06-11 14:15:55 +0000

    :sparkles: created thirteenth_commit
    today learned

---
+++ Linux/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("好きな数字を入力してください");
+    int i = new java.util.Scanner(System.in).nextInt();
+    if (i % 15 == 0) {
+      System.out.println("FizzBuzz");
+    } else if(i % 3 == 0) {
+      System.out.println("Fizz");
+    } else if (i % 5 == 0){
+      System.out.println("Buzz");
+    } else {
+      
+    }
+  }
+}
